66e3ba82f86f9e0f3bd6046321ee8c92b54c3f50
mpromed <86148410+mpromed@users.noreply.github.com>
2024-10-31 14:20:37 +0300

    Add files via upload

---
+++ assets/ContentText-d8e2d7f6.js
@@ -0,0 +1,1 @@
+import{u as o,o as s,c}from"./index-455d3f20.js";const a=["innerHTML"],l={__name:"ContentText",props:["componentData","testStyle"],setup(t){const e=t;return o(n=>({"64fbb36c":t.testStyle.textColor})),(n,_)=>(s(),c("div",{innerHTML:e.componentData.text,class:"content-text"},null,8,a))}};export{l as default};
