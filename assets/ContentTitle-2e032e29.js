cb6688996dc8aed880212863f4fe1ac76709005a
mpromed <86148410+mpromed@users.noreply.github.com>
2025-01-31 12:08:17 +0300

    Add files via upload

---
+++ assets/ContentTitle-2e032e29.js
@@ -0,0 +1,1 @@
+import{u as o,o as s,c as a}from"./index-6c82e962.js";const c=["innerHTML"],r={__name:"ContentTitle",props:["componentData","testStyle"],setup(t){const e=t;return o(n=>({"8250a562":t.testStyle.titleColor})),(n,l)=>(s(),a("div",{innerHTML:e.componentData.title,class:"content-title"},null,8,c))}};export{r as default};
